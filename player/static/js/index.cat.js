4b12d5ece646eee9ea0d1e00074960df350356a7
lcm <814114682@qq.com>
2014-12-11 22:14:43 +0800

    fix

--- player/static/js/index.cat.js
+++ player/static/js/index.cat.js
@@ -175,7 +175,7 @@ playerApp.directive('musicMode', ['$timeout', '$document', function($timeout, $d
 			player : '=',
 			audio: '='
 		},
-		templateUrl: '/tpls/mode.html',
+		templateUrl: 'tpls/mode.html',
 		link: function(scope, ele, attr) {
 
 			var status = 'list'; //标识播放模式，默认为list
@@ -281,7 +281,7 @@ playerApp.directive('progressBar', ['$document', '$interval', '$rootScope', func
 			player: '=',
 			audio: '='
 		},
-		templateUrl: '/tpls/progress.html',
+		templateUrl: 'tpls/progress.html',
 		link: function(scope, rootScope, ele, attr) {
 			console.log($rootScope);
 			scope.surplusBar = function() { //音乐剩余时间
